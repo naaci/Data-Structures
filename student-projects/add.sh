1db1483adae83e8007c7f618589da57d949f86fb
Naci Er <naci.er@alanyauniversity.edu.tr>
2023-12-29 17:24:56 +0300

    students works

---
+++ student-projects/add.sh
@@ -0,0 +1,16 @@
+git submodule add https://github.com/oguzuo/Implement-a--ISO9660-File-System-Reader.git 200029007 
+git submodule add https://github.com/enesersoz/ISO-9660Reader.git 210029003 
+git submodule add https://github.com/ysrhuseyin/ISO9660-reader.git 210029005 
+git submodule add https://github.com/ibrahimercosman/odevv.py.git 210029007 
+git submodule add https://github.com/Mozyurek/ISO-9660reader.git 210029009 
+git submodule add https://github.com/izgi-gkcnr/pythonProject5.git 210029012 
+git submodule add https://github.com/ozanokutan/DataStructures.git 210029015 
+git submodule add https://github.com/HasanK07/ISO9660-READER-PROGRAM.gi 210029016 
+git submodule add https://github.com/Ahmet417/odev.py.git 210029017 
+git submodule add https://github.com/uurktk/ISO9960FileReaderSystem.git 210029018 
+git submodule add https://github.com/Ayberkb1/dataproject.git 210029019 
+git submodule add https://github.com/iskburcin/ISO9660Reader.git 210029021 
+git submodule add https://github.com/ChilL-Q/iso9960parser.git 220029801 
+git submodule add https://github.com/ArtemParshyn/iso9960_reader.git 220029802 
+git submodule add https://github.com/LiyanaKh/ISO9660.git 220029803 
+git submodule add https://github.com/imranborzov/iso9960reader1.git 220029811 
